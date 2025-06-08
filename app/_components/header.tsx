da5f9924ff12711539279c670811418b8931345b
PRIYANSH NARANG <priyanshnarang@PRIYANSHs-MacBook-Air.local>
2025-06-08 16:57:10 +0530

    removeed

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -25,7 +25,6 @@ const Header = () => {
   const menuList = [
     { name: 'Home', path: '/' },
     { name: 'Create Story', path: '/create-story' },
-    { name: 'Contact', path: '/contact' },
   ]
 
   return (
